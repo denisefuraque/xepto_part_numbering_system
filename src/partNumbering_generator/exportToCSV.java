7e7ee788e7ff117eba62c575b9240ee2b6d4794c
Denise Furaque <denisefuraque@gmail.com>
2017-07-25 13:14:52 +0800

    add exportToCSV;

--- src/partNumbering_generator/exportToCSV.java
+++ src/partNumbering_generator/exportToCSV.java
@@ -1,17 +1,70 @@
 package partNumbering_generator;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Denise Furaque
  */
 public class exportToCSV {
-
+    
+    static private String host_address;
+    
+    public exportToCSV(){
+        
+    }
+    
+    public void export(){
+        
+        try{
+            
+            Scanner s = new Scanner(new FileReader("host.txt"));
+            host_address = s.nextLine();
+            
+            String name = "C:\\Users\\Denise Furaque\\Desktop\\part_number_csv.csv";
+            try{
+                FileWriter fw = new FileWriter(name);
+                
+                
+                
+                Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+                Connection conn = DriverManager.getConnection("jdbc:derby://" + host_address + "/partNumbering  ", "Admin01", "07032017");
+                String query  = "SELECT * FROM PART_NUMBER_DATA";
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                while(rs.next())
+                {
+                    fw.append(rs.getString(1));
+                    fw.append(',');
+                    fw.append(rs.getString(2));
+                    fw.append(',');
+                    fw.append(rs.getString(3));
+                    fw.append(',');
+                    fw.append(rs.getString(4));
+                    fw.append(',');
+                    fw.append(rs.getString(5));
+                    fw.append(',');
+                    fw.append(rs.getString(6));
+                    fw.append('\n');
+                }
+                fw.flush();
+                fw.close();
+                conn.close();
+                System.out.println("CSV File is created successfully.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(exportToCSV.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
 }
